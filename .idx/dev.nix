254a51c874fa9a09a30ee21235cd670a99966b83
Eder Oliveira <edersign@gmail.com>
2025-10-02 12:03:03 -0300

    Update Node.js package version in dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,7 +1,7 @@
 {pkgs}: {
   channel = "stable-24.05";
   packages = [
-    pkgs.nodejs_latest
+    pkgs.nodejs_24
   ];
   idx.extensions = [ "bradlc.vscode-tailwindcss" "dbaeumer.vscode-eslint" "esbenp.prettier-vscode" "rvest.vs-code-prettier-eslint" "YoavBls.pretty-ts-errors"];
   idx.previews = {
