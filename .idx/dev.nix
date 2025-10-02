a036aacf8b9d678f4a8f3a758565e09d0fb5d68d
edersign <edersign@gmail.com>
2025-10-02 14:45:12 +0000

    dev.nix upgrade node version

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,7 +1,7 @@
 {pkgs}: {
   channel = "stable-24.05";
   packages = [
-    pkgs.nodejs_22
+    pkgs.nodejs_latest
   ];
   idx.extensions = [ "bradlc.vscode-tailwindcss" "dbaeumer.vscode-eslint" "esbenp.prettier-vscode" "rvest.vs-code-prettier-eslint" "YoavBls.pretty-ts-errors"];
   idx.previews = {
